7d7b51944c1e64b336b09a924034d37a6510859d
rtie2002 <raymond.tie.rt@gmail.com>
2026-01-08 21:33:39 +0800

    w

---
+++ Data_filtering/algorithm1_v2_multivariate.ps1
@@ -0,0 +1,14 @@
+python Data_filtering/algorithm1_v2_multivariate.py --appliance_name fridge --clip_max 250
+
+# Microwave
+python Data_filtering/algorithm1_v2_multivariate.py --appliance_name microwave
+
+# Kettle
+python Data_filtering/algorithm1_v2_multivariate.py --appliance_name kettle
+
+# Dishwasher
+python Data_filtering/algorithm1_v2_multivariate.py --appliance_name dishwasher
+
+# Washing Machine
+python Data_filtering/algorithm1_v2_multivariate.py --appliance_name washingmachine
+```
