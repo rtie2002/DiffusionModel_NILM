1ed080256fbb70ce960f36faa886d1b751e85b82
rtie2002 <raymond.tie.rt@gmail.com>
2026-01-08 18:37:02 +0800

    11

---
+++ preprocess_NILMformer/run_all_preprocessing.ps1
@@ -0,0 +1,29 @@
+# Fridge (冰箱)
+python preprocess_NILMformer\multivariate_ukdale_preprocess_testing.py --appliance_name fridge --data_dir "NILM-main/dataset_preprocess/UK_DALE/"
+
+# Microwave (微波炉)
+python preprocess_NILMformer\multivariate_ukdale_preprocess_testing.py --appliance_name microwave --data_dir "NILM-main/dataset_preprocess/UK_DALE/"
+
+# Kettle (水壶)
+python preprocess_NILMformer\multivariate_ukdale_preprocess_testing.py --appliance_name kettle --data_dir "NILM-main/dataset_preprocess/UK_DALE/"
+
+# Dishwasher (洗碗机)
+python preprocess_NILMformer\multivariate_ukdale_preprocess_testing.py --appliance_name dishwasher --data_dir "NILM-main/dataset_preprocess/UK_DALE/"
+
+# Washing Machine (洗衣机)
+python preprocess_NILMformer\multivariate_ukdale_preprocess_testing.py --appliance_name washingmachine --data_dir "NILM-main/dataset_preprocess/UK_DALE/"
+
+# Fridge (冰箱)
+python preprocess_NILMformer\multivariate_ukdale_preprocess_training+validation.py --appliance_name fridge --data_dir "NILM-main/dataset_preprocess/UK_DALE/"
+
+# Microwave (微波炉)
+python preprocess_NILMformer\multivariate_ukdale_preprocess_training+validation.py --appliance_name microwave --data_dir "NILM-main/dataset_preprocess/UK_DALE/"
+
+# Kettle (水壶)
+python preprocess_NILMformer\multivariate_ukdale_preprocess_training+validation.py --appliance_name kettle --data_dir "NILM-main/dataset_preprocess/UK_DALE/"
+
+# Dishwasher (洗碗机)
+python preprocess_NILMformer\multivariate_ukdale_preprocess_training+validation.py --appliance_name dishwasher --data_dir "NILM-main/dataset_preprocess/UK_DALE/"
+
+# Washing Machine (洗衣机)
+python preprocess_NILMformer\multivariate_ukdale_preprocess_training+validation.py --appliance_name washingmachine --data_dir "NILM-main/dataset_preprocess/UK_DALE/"
